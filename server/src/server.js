e5df4f3e1e91ec763131179ee93298d0a1820604
KunjJoshi <iamkunjjoshi@gmail.com>
2026-04-16 16:59:04 -0400

    CORS Removal + Likert Scale

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import session from "express-session";
 import passport from "passport";
@@ -18,7 +17,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.static("client"));
 
